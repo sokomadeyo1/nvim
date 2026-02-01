8e790318a3c8a09edb77cc7bf8418195e6b18f8b
Yuri Tihonov <tihonov-yuri@mail.ru>
2026-02-01 21:23:49 +0300

    Update snippet keymaps

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,8 +1,8 @@
 local ls = require("luasnip")
 
 vim.keymap.set({ "i" }, "<C-J>", function() ls.expand() end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<Tab>", function() ls.jump(1) end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<S-Tab>", function() ls.jump(-1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-H>", function() ls.jump(-1) end, { silent = true })
 
 vim.keymap.set({ "i", "s" }, "<C-Tab>", function()
   if ls.choice_active() then
